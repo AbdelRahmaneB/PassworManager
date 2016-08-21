0f4cf7b9f730195e1ccaee457951a9cbb3953690
UcefBen <ucef-ben@hotmail.fr>
2016-08-21 11:24:17 -0400

    Initialization commit - First commit

---
+++ app/src/main/java/ub/passwordmanager/SplashScreen.java
@@ -0,0 +1,37 @@
+package ub.passwordmanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Description : Splash Screen
+ * In this class we will decide if we have to send the user to
+ * the login view or the sign in view
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        // Set the timer for 2 sec
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 2000);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
