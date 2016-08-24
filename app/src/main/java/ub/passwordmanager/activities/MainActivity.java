16759c99c5e4a7dadd13131e9a55f4d9a03b4360
UcefBen <ucef-ben@hotmail.fr>
2016-08-23 22:42:21 -0400

    IHM Module : Home Page - part 1
    Creation of a container layout, Content layout, and the menu drawer. Added some icons for the menu, plus the action for each item

--- app/src/main/java/ub/passwordmanager/activities/MainActivity.java
+++ app/src/main/java/ub/passwordmanager/activities/MainActivity.java
@@ -1,15 +1,117 @@
 package ub.passwordmanager.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import ub.passwordmanager.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add the customized action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Add the even handler for the floating button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Add the navigation menu drawer to the view
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Add the listener for our navigation menu
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+
+    /**
+     * This function is derived from the "NavigationView.OnNavigationItemSelectedListener".
+     * it allow us to add the listener for when an item is selected in the menu
+     *
+     * @param item : the selected item in the menu
+     * @return True or false depending if we found the desired menu
+     */
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+
+            case R.id.nav_home:
+                Toast.makeText(getBaseContext(), "Home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_profile:
+                Toast.makeText(getBaseContext(), "Profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_import_export:
+                Toast.makeText(getBaseContext(), "Import/Export", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_password_generator:
+                Toast.makeText(getBaseContext(), "Password Generator", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(getBaseContext(), "Setting", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_About:
+                Toast.makeText(getBaseContext(), "About", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                return false;
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    /**
+     * Redefine the @onBackPressed method
+     * so that if the menu is open and we press the back button
+     * we close the menu drawer, or in the other case we go back to the previous fragment
+     */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            // TODO : insert the code to manage the back action between fragment
+            super.onBackPressed();
+        }
+    }
+
 }
