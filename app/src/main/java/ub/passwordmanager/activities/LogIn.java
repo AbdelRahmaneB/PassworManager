70d73b3bc67583518a0bfcbfe8e54b7bad8b5e08
UcefBen <ucef-ben@hotmail.fr>
2016-08-28 20:11:46 -0400

    IHM Module : LogIn Details
    Add some tests on the filed

--- app/src/main/java/ub/passwordmanager/activities/LogIn.java
+++ app/src/main/java/ub/passwordmanager/activities/LogIn.java
@@ -2,9 +2,12 @@ package ub.passwordmanager.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,32 +16,26 @@ import ub.passwordmanager.R;
 
 public class LogIn extends AppCompatActivity {
 
+    private EditText tv_pwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-//        final ImageView iv = (ImageView) findViewById(R.id.iv_my_visibility_login);
-//        iv.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getBaseContext(), "Button", Toast.LENGTH_SHORT).show();
-//                if ("Visible".equals(iv.getTag().toString())) {
-//                    iv.setBackgroundResource(R.drawable.ic_visibility_off);
-//                    iv.setTag("NotVisible");
-//                }else{
-//                    iv.setBackgroundResource(R.drawable.ic_visibility);
-//                    iv.setTag("Visible");
-//                }
-//
-//            }
-//        });
+        tv_pwd = (EditText) findViewById(R.id.t_password_login);
 
-        Button logiButton = (Button) findViewById(R.id.bt__login);
-        logiButton.setOnClickListener(new View.OnClickListener() {
+        Button logInButton = (Button) findViewById(R.id.bt__login);
+        logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                if (!TextUtils.isEmpty(tv_pwd.getText().toString())) {
+                    // ToDo : Test the login and password
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                }else{
+                    TextInputLayout pwd = (TextInputLayout) findViewById(R.id.input_pwd_login);
+                    pwd.setError(getResources().getString(R.string.logIn_empty_password));
+                }
+
             }
         });
 
